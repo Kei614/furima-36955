352be17d771096684e718b4315472f123915ef48
Kei614 <kei.9087@gmail.com>
2022-01-06 22:07:50 +0900

    #4 ユーザー管理機能

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々]+\z/
+  validates :first_name,         presence: true, format: { with: NAME_REGEX, message: 'は全角(漢字、ひらがな、カタカナ)で入力してください' }
+  validates :last_name,          presence: true, format: { with: NAME_REGEX, message: 'は全角(漢字、ひらがな、カタカナ)で入力してください' }
+  NAME_KANA_REGEX = /\A[ァ-ヶ]+\z/
+  validates :first_name_in_kana, presence: true, format: { with: NAME_KANA_REGEX, message: 'はカタカナ(全角)で入力してください' }
+  validates :last_name_in_kana,  presence: true, format: { with: NAME_KANA_REGEX, message: 'はカタカナ(全角)で入力してください' }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含むようにしてください' }
+  validates :date_of_birth,      presence: true
+end
