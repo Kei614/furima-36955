201e99eb06d0a43d4862a89de5b57b84f6ba4226
Kei614 <kei.9087@gmail.com>
2022-01-12 20:51:23 +0900

    #7商品情報編集機能　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    # @item = Item.find(params[:id])
   end
 
   def edit
-    # @item = Item.find(params[:id])
   end
 
   def update
-    # @item = Item.find(params[:id])
     if @item.update(item_parameter)
       redirect_to item_path(@item.id)
     else
