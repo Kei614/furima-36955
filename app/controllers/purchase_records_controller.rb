63de7a073a0378702effde72840aae7e0fd1af6a
Kei614 <kei.9087@gmail.com>
2022-01-20 02:38:28 +0900

    #9商品購入機能

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -5,24 +5,25 @@ class PurchaseRecordsController < ApplicationController
   
 
   def index
-    binding.pry
     @purchase_record_destination = PurchaseRecordDestination.new
   end
 
   def create
     @purchase_record_destination = PurchaseRecordDestination.new(purchase_record_params)
     if @purchase_record_destination.valid?
+      pay_item
       @purchase_record_destination.save
       redirect_to root_path
     else
       render :index
     end
+
   end
 
   private
 
   def purchase_record_params
-    params.require(:purchase_record_destination).permit(:zip_code,:prefecture_id,:city,:address,:building,:telephone_number).merge(user_id: current_user.id,item_id: params[:item_id])
+    params.require(:purchase_record_destination).permit(:zip_code,:prefecture_id,:city,:address,:building,:telephone_number).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token],price: @item.price)
   end
 
   def set_item
@@ -30,6 +31,18 @@ class PurchaseRecordsController < ApplicationController
   end
 
   def move_to_root
-    redirect_to root_path if current_user.id == @item.user.id
+    if current_user.id == @item.user.id || @item.purchase_record.present?
+      redirect_to root_path
+    end 
   end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount:   purchase_record_params[:price],
+      card:     purchase_record_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
--- app/models/purchase_record_destination.rb
+++ app/models/purchase_record_destination.rb
@@ -1,17 +1,19 @@
 class PurchaseRecordDestination
   include ActiveModel::Model 
-  attr_accessor :item_id, :user_id, :zip_code, :prefecture_id, :city,:address, :building, :telephone_number, :hoge
+  attr_accessor :item_id, :user_id, :zip_code, :prefecture_id, :city,:address, :building, :telephone_number, :token, :price
   
   # バリデーション
   with_options presence: true do
     validates :item_id
     validates :user_id
-    validates :zip_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "は、ハイフンを含めて記載してください"}
-    validates :prefecture_id, numericality: {other_than: 1, message: "を選択してください"}
+    validates :zip_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "は、ハイフンを含めて半角数字で入力してください。例：111-1111"}
     validates :city
     validates :address
-    validates :telephone_number,format:{ with:/\A[0-9]+\z/,message: "は、数字のみで入力してください"},length: { in: 10..11}
+    validates :telephone_number,format:{ with:/\A[0-9]+\z/,message: "は、半角数字のみで10~11文字で入力してください"},length: { in: 10..11}
+    validates :price
+    validates :token
   end
+  validates :prefecture_id, numericality: {other_than: 1, message: "を選択してください"}
 
   def save
     purchase_record = PurchaseRecord.create(item_id: item_id,user_id: user_id)
